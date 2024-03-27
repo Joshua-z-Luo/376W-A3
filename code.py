bca1a79776eb3446c6147baf81bc771883362ec3
Joshua Luo <42794203+Joshua-z-Luo@users.noreply.github.com>
2024-03-26 21:17:18 -0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,45 @@
+#allows us to use the turtles library
+import turtle           
+
+#creates a graphics window
+window = turtle.Screen()       
+
+#creates a turtle called john
+john = turtle.Turtle()
+
+john.forward(150)
+john.left(120)
+
+john.forward(150)
+john.left(120)
+
+john.forward(150)
+
+#allows us to use the turtles library
+import turtle           
+
+#creates a graphics window
+window = turtle.Screen()       
+
+#creates a turtle called john
+john = turtle.Turtle()
+
+john.forward(150)
+john.left(90)
+
+john.forward(150)
+
+john.left(90)
+
+john.forward(150)
+john.left(90)
+
+john.forward(150)
+
+
+a = 5
+john = "happy"
+
+b = 12
+john = "sad"
+print(a+b)
